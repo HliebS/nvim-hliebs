3c61671b2f24679d7b2609a4296558e759b5e133
HliebS <129861638+HliebS@users.noreply.github.com>
2023-04-06 11:59:37 +0200

    Add remap definitions, references

--- lua/HliebS/remap.lua
+++ lua/HliebS/remap.lua
@@ -1,15 +1,24 @@
+-- Set leader and leader in normal mode
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- ? : ThePrimeagen
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- ? : ThePrimeagen
 vim.keymap.set("n", "J", "mzJ`z")
+
+-- Keymaps for navigating the file vertically by half with centering of cursor
+-- :ThePrimeagen
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Set keymap for search centering and selection : ThePrimeagen
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+-- ? : ThePrimeagen
 vim.keymap.set("n", "<leader>vwm", function()
     require("vim-with-me").StartVimWithMe()
 end)
@@ -17,33 +26,39 @@ vim.keymap.set("n", "<leader>svwm", function()
     require("vim-with-me").StopVimWithMe()
 end)
 
--- greatest remap ever
+-- Keymap for multiple pasting of same yank : ThePrimeagen
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- next greatest remap ever : asbjornHaland
+-- ? : asbjornHaland
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
+-- ? : ThePrimeagen
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
--- This is going to get me cancelled
+-- Keymap setting Ctr+c to Esc to exit modes : ThePrimeagen
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
+-- ? : ThePrimeagen
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
+-- ? : ThePrimeagen
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
+-- ? : ThePrimeagen
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
+-- ? : ThePrimeagen
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
+-- Keymap to shout-out with double space : ThePrimeagen
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
