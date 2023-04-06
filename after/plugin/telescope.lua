7fc8857cb7b6facef284f246c4b999d4f04de572
HliebS <129861638+HliebS@users.noreply.github.com>
2023-04-06 14:56:21 +0200

    Add comments for telescope remaps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,12 @@
 local builtin = require('telescope.builtin')
+
+-- Keymap to open file search : ThePrimeagen
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+
+-- Keymap to open git file search : ThePrimeagen
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+
+-- Keymap to open a grep search : ThePrimeagen
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
